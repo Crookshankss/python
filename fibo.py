b57f80ed4b864c774022a388345cb3b53620cae5
Ahana Swathy <67807452+Crookshankss@users.noreply.github.com>
2021-02-14 21:53:45 +0530

    Add files via upload

---
+++ fibo.py
@@ -0,0 +1,21 @@
+def fib(n):
+
+    a = 0
+    b = 1
+
+    
+    
+    if n < 0:
+        print('error')
+    else:
+     print(a)
+     print(b)
+
+    for i in range(2,n):
+        c = a + b
+        a = b
+        b = c
+        print(a+b)
+        
+        
+fib(8)
